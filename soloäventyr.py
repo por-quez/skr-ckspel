3950dff080e8afe6758d0f7610616d2d49bc746c
por-quez <sander.anderssonmill@elev.ga.ntig.se>
2024-12-17 13:49:16 +0100

    menu

--- soloäventyr.py
+++ soloäventyr.py
@@ -1,21 +1,33 @@
 import random
 
+#Start
 print("Welcome to the solo adventure, Slitherin escapees!\nHow would you like to proceed?")
+
 print("1. Play\n2. Options\n3. Quit")
-int(input("")) == menuopt
 
-if menuopt == "1":
+menuopt = int(input())
+
+#Start Menu
+if menuopt == 1:
+
+    print(menu2)
+
+elif menuopt == 2:
+
+    print(options)
+
+elif menuopt == 3:
     
+    quit()
 
-player_health = 100
+#Game Menu
+def game_extract():
+    return "1. New Game\n2. Continue\n. Tutorial"
 
+menu2 = print(game_extract())
 
-print("You wake up in a dark cell, you can't make out where you are but you see a door with a dimm light. You get 2 options, inspect the cell or inspect the door.")
+#Options
+def options_def():
+    return "1. Sound\n2. Text size\n3. Gore"
 
-cellopt = int(input("1 or 2"))
-
-if cellopt == 1:
-    print("In the cell you notice a loose toilet and a bed with a compartement under it, would you like to inspect the toilet or under the bed? Put: Toilet or Bed.")
-
-elif cellopt == 2:
-    print("You inspect the door and shout for help to see if anyone is out there, but instead you are met with a fierce human head that throws you off-guard. You hurridly pull yourself back as a man with a human face on him is already with haste walking towards you. He lifts you up with immense strength and bashes your head into the stone cold wall. You have fallen, Game over!")
+options = print(options_def())
